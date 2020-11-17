7de76a5ff638d201c7a6e6b4571e59b6b11d9ceb
zhouguodong <18513080058@163.com>
2020-11-17 14:31:06 +0800

    添加系统配置

--- tcc-rest-api/src/main/java/com/tcc/example/tcc/rest/api/TccRestApiApplication.java
+++ tcc-rest-api/src/main/java/com/tcc/example/tcc/rest/api/TccRestApiApplication.java
@@ -2,8 +2,12 @@ package com.tcc.example.tcc.rest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TccRestApiApplication {
 
     public static void main(String[] args) {
